abe9fe27149c89172313b3e91e6d38cf30bdcec5
mcreng <tsehonam@gmail.com>
2019-02-05 23:20:26 +0800

    dev: Firestore upsert workaround

--- firestore.js
+++ firestore.js
@@ -11,6 +11,25 @@ const config = {
   messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
 };
 
+/**
+ * Upserts fields in the document referred to by this `DocumentReference`.
+ *
+ * @param data An object containing the fields and values with which to
+ * upsert the document. Fields can contain dots to reference nested fields
+ * within the document.
+ * @return A Promise resolved once the data has been successfully written
+ * to the backend (Note that it won't resolve while you're offline).
+ */
+firebase.firestore.DocumentReference.prototype.upsert = function(data) {
+  this.get().then(docSnapshot => {
+    if (docSnapshot.exists) {
+      return this.update(data);
+    } else {
+      return this.set(data);
+    }
+  });
+};
+
 const app = firebase.initializeApp(config);
 const firestore = firebase.firestore(app);
 
