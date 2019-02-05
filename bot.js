c412251c6e220bf387aa6c1d3ac77f9441f16d10
mcreng <tsehonam@gmail.com>
2019-02-05 23:21:04 +0800

    dev: Allow user to add item to db through /remind

--- bot.js
+++ bot.js
@@ -1,8 +1,25 @@
 require("dotenv").config();
 const Telegraf = require("telegraf");
+const Extra = require("telegraf/extra");
+
+const firebase = require("firebase/app");
 const firestore = require("./firestore");
 
 const bot = new Telegraf(process.env.TG_BOT_TOKEN);
+
+/**
+ * Get session key of current session. Used as unique key in database.
+ * @param ctx Context for Telegram update
+ * @return {str} Session Key
+ */
+const getSessionKey = ctx => {
+  if (ctx.from && ctx.chat) {
+    return `${ctx.from.id}:${ctx.chat.id}`;
+  } else if (ctx.from && ctx.inlineQuery) {
+    return `${ctx.from.id}:${ctx.from.id}`;
+  }
+  return null;
+};
 
 /**
  * /start command.
@@ -15,10 +32,75 @@ bot.start(ctx => ctx.reply("Welcome!"));
 bot.help(ctx => ctx.reply("Hi!"));
 
 /**
- * /reply command.
+ * /remind command.
+ *
+ * Upon calling, toEnterReminder state is set, and user is expected to either
+ * enter the item to remind next, or cancel the action.
  */
-bot.command("/remind", ctx =>
-  ctx.reply("What do you want me to remind you of?")
-);
+bot.command("/remind", ctx => {
+  ctx.reply(
+    "What do you want me to remind you of?\n\nReply me the item to be reminded of, or cancel the action.",
+    Extra.markup(m =>
+      m.inlineKeyboard([m.callbackButton("Cancel", "cancelEnterReminder")])
+    )
+  );
+  firestore
+    .collection("states")
+    .doc(getSessionKey(ctx))
+    .upsert({ toEnterReminder: true });
+});
+
+/**
+ * Text Listener.
+ *
+ * If toEnterReminder is set, bot listens to reminder item. As user reply bot
+ * with item, the item is stored in database.
+ * Otherwise, the text is discarded.
+ */
+bot.on("text", ctx => {
+  firestore
+    .collection("states")
+    .doc(getSessionKey(ctx))
+    .get()
+    .then(docSnapshot => {
+      // check toEnterReminder state
+      if (docSnapshot.exists && docSnapshot.data()["toEnterReminder"]) {
+        firestore
+          .collection("states")
+          .doc(getSessionKey(ctx))
+          .upsert({ toEnterReminder: false });
+
+        let remindTime = Date.now(); // TODO: hard-coded for now
+
+        firestore
+          .collection("reminders")
+          .doc(getSessionKey(ctx))
+          .upsert({
+            items: firebase.firestore.FieldValue.arrayUnion({
+              name: ctx.message.text,
+              time: remindTime
+            })
+          });
+
+        // TODO: Reformat text with proper grammar
+        ctx.reply(
+          `I will remind you of "${ctx.message.text}" at ${remindTime}.`
+        );
+      }
+    });
+});
+
+/**
+ * Cancel Enter Reminder action.
+ *
+ * Deletes the message, and reset toEnterReminder state.
+ */
+bot.action("cancelEnterReminder", ctx => {
+  ctx.deleteMessage();
+  firestore
+    .collection("states")
+    .doc(getSessionKey(ctx))
+    .upsert({ toEnterReminder: false });
+});
 
 module.exports = bot;
